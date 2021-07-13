a1bca54a9b238cc3e3a381b1da904e41b2d0c801
paulobocanegra <paulobocanegra@gmail.com>
2021-07-13 11:57:59 -0700

    fixed syntax

--- validation/review.js
+++ validation/review.js
@@ -1,7 +1,7 @@
 const Validator = require('validator');
 const validText = require('./valid-text');
 
-module.exports = function validateTweetInput(data) {
+module.exports = function validateReviewInput(data) {
     let errors = {};
 
     data.name = validText(data.name) ? data.name : '';
